4a465100610a2312847be65c14a5f1d7471f58dc
TomCP20 <107045601+TomCP20@users.noreply.github.com>
2026-09-16 17:22:39 +0100

    Created Project Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './style.css'
 import ThreeDGameOfLife from '/3DGameOfLife.png'
-import { Link } from './Link'
+import { Project } from './Project'
 
 function App() {
 
@@ -18,34 +18,26 @@ function App() {
         </p>
       </div>
       <h2 className='text-center text-xl m-4'>Projects</h2>
-      <div className='w-[50%] m-auto p-1'>
-        <h3 className='text-center text-lg m-4'>Cellular Automaton Collection</h3>
-        <img src={ThreeDGameOfLife} alt='3DGameOfLife' className='size-100 bg-black mx-auto my-8' />
-        <div>
-          Cellular Automaton Collection is a website that contains various celluar automaton such as:
-          <ul className="list-disc list-inside">
-            <li>Wolfram code</li>
-            <li>Conway's Game of Life</li>
-            <li>Maze and Mazectric</li>
-            <li>The 3D Game of Life</li>
-          </ul>
-          <br />
-          The Website was created using:
-          <ul className="list-disc list-inside">
-            <li>React</li>
-            <li>Typescript</li>
-            <li>Tailwind CSS</li>
-            <li>Three.js</li>
-            <li>React Three Fiber</li>
-          </ul>
-        </div>
-        <div className='text-center m-4'>
-          <Link to='https://github.com/TomCP20/Cellular-Automaton-Collection'>Repo</Link> <Link to='https://github.com/TomCP20/Cellular-Automaton-Collection'>Page</Link>
-        </div>
-      </div>
+      <Project title='Cellular Automaton Collection' image={ThreeDGameOfLife} repoTo='https://github.com/TomCP20/Cellular-Automaton-Collection' pageTo='https://tomcp20.github.io/Cellular-Automaton-Collection/'>
+        Cellular Automaton Collection is a website that contains various celluar automaton such as:
+        <ul className="list-disc list-inside">
+          <li>Wolfram code</li>
+          <li>Conway's Game of Life</li>
+          <li>Maze and Mazectric</li>
+          <li>The 3D Game of Life</li>
+        </ul>
+        <br />
+        The Website was created using:
+        <ul className="list-disc list-inside">
+          <li>React</li>
+          <li>Typescript</li>
+          <li>Tailwind CSS</li>
+          <li>Three.js</li>
+          <li>React Three Fiber</li>
+        </ul>
+      </Project>
     </>
   )
 }
 
 export default App
-
---
+++ src/Project.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from 'react';
+import { Link } from './Link';
+
+export function Project({ title, image, repoTo, pageTo, children }: Readonly<{ title: string; image: string; repoTo: string; pageTo: string; children: ReactNode; }>) {
+  return (
+    <div className='w-[50%] m-auto p-1'>
+      <h3 className='text-center text-lg m-4'>{title}</h3>
+      <img src={image} className='size-100 bg-black mx-auto my-8' />
+      <div>
+        {children}
+      </div>
+      <div className='text-center m-4'>
+        <Link to={repoTo}>Repo</Link> <Link to={pageTo}>Page</Link>
+      </div>
+    </div>);
+}
