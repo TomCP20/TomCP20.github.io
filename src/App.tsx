82309a67420e5b98e890262ecc70312ef42ed638
TomCP20 <107045601+TomCP20@users.noreply.github.com>
2026-09-16 16:08:59 +0100

    Added Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import './style.css'
 
 function App() {
   const [count, setCount] = useState(0)
