2f337c7c9cd5ace28503ed7569b4f58575ffa990
TomCP20 <107045601+TomCP20@users.noreply.github.com>
2026-09-20 05:17:50 +0100

    refactored table of contents

--- src/TableOfContents.tsx
+++ src/TableOfContents.tsx
@@ -1,90 +1,48 @@
 import { useEffect, useState } from 'react';
 
 export default function TableOfContents() {
-    const { nestedHeadings } = useHeadingsData();
-    console.log(nestedHeadings)
+    const { headings } = useHeadingsData();
     return (
         <nav aria-label='Table of contents' className='h-screen sticky top-0 py-1 px-2 float-right'>
-            <Headings headings={nestedHeadings} />
+            <ul className='list-disc list-inside'>
+                {headings.map((heading) => <HeaderLink heading={heading} />)}
+            </ul>
         </nav>
     )
 }
 
-function Headings({ headings }: Readonly<{ headings: outerHeader[] }>) {
+function HeaderLink({ heading }: Readonly<{ heading: header }>) {
     return (
-        <ul className='list-disc list-inside'>
-            {headings.map((heading) => (
-                <li key={heading.id}>
-                    <HeaderLink heading={heading} />
-                    {heading.items && (
-                        <ul className='list-disc list-inside indent-4'>
-                            {heading.items.map((child) => (
-                                <li key={child.id}>
-                                    <HeaderLink heading={child} />
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </li>
-            ))}
-        </ul>
-    )
+        <li key={heading.id} className={heading.isChild ? "indent-4" : ""}>
+            <a href={`#${heading.id}`} className='hover:underline' >
+                {heading.title}
+            </a>
+        </li >
+    );
 }
 
-function HeaderLink({ heading }: Readonly<{ heading: innerHeader | outerHeader }>) {
-    return <a
-        href={`#${heading.id}`}
-        onClick={(e) => {
-            e.preventDefault();
-            document.querySelector(`#${heading.id}`)?.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }}
-        className='hover:underline'
-    >{heading.title}</a>;
-}
 
 function useHeadingsData() {
-    const [nestedHeadings, setNestedHeadings] = useState<outerHeader[]>([]);
+    const [headings, setHeadings] = useState<header[]>([]);
 
     useEffect(() => {
+        const newHeadings: header[] = [];
+
         const headingElements = Array.from(
             document.querySelectorAll<HTMLElement>('h2, h3')
         );
 
-        const newNestedHeadings = getNestedHeadings(headingElements);
-        setNestedHeadings(newNestedHeadings);
+        headingElements.forEach((heading) => {
+            newHeadings.push({ id: heading.id, title: heading.innerText, isChild: heading.nodeName === 'H3' });
+        });
+        setHeadings(newHeadings);
     }, []);
 
-    return { nestedHeadings };
+    return { headings };
 };
 
-function getNestedHeadings(headingElements: HTMLElement[]) {
-    const nestedHeadings: outerHeader[] = [];
-
-    headingElements.forEach((heading) => {
-        const { innerText: title, id } = heading;
-
-        if (heading.nodeName === 'H2') {
-            nestedHeadings.push({ id, title, items: [] });
-        } else if (heading.nodeName === 'H3' && nestedHeadings.length > 0) {
-            nestedHeadings[nestedHeadings.length - 1].items.push({
-                id,
-                title,
-            });
-        }
-    });
-
-    return nestedHeadings;
-};
-
-interface innerHeader {
+interface header {
     id: string
     title: string
-}
-
-interface outerHeader {
-    id: string
-    title: string
-    items: innerHeader[]
+    isChild: boolean
 }
